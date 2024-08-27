c9104316b852f3252a3a9335a6855ef849c5bfe2
ASChavis <126298472+ASChavis@users.noreply.github.com>
2024-08-27 15:32:38 -0400

    Start of Project
    Initialized React. Created componet folders. Started creating the input form.

---
+++ pet-food-calculator/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Pet Food Calculator!</h1>
+      <h3>To get started, enter your pet's information below</h3>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ pet-food-calculator/src/componets/input.js
@@ -0,0 +1,10 @@
+function inputForm(){
+return(
+<div>
+<p>This is working</p>
+</div>
+
+)
+};
+
+export default inputForm;
